e079837bb334db36b84145e2b178a9ef50289db5
thaisflx <thais.felix@etudiant.univ-reims.fr>
2025-10-22 16:33:40 +0200

    Delete wr313 directory

--- wr313/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mes Exos de POO</title>
-    <meta charset="utf-8" />
-</head>
-<body>
-<h1>Mes Exos de POO</h1>
-</body>
--- wr313/seance8/iHumain.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface iHumain {
-    public function travailler(float $nombreHeures): void;
-    public function reposer(int $nombreJours): void;
-    public function sePresente(): string;
-}
-?>
-
